6a7220efd50fff03b015b091f8768ce791185c47
Preedee@Digital.Dev <nickmsft@mac.com>
2025-09-18 07:19:18 +0700

    Increase connection pool size and add request logging middleware
    - Updated `main.rs` to double `PgPool` connection pool size (5 to 10).
    - Integrated Actix Web's logging middleware for better request visibility.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use log::info;
 use pretty_env_logger;
 use perf_dashboard::interface::api;
 use actix_files as fs;
-use actix_files::Files;
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -15,7 +15,7 @@ async fn main() -> std::io::Result<()> {
     info!("Starting server...");
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = PgPoolOptions::new()
-        .max_connections(5)
+        .max_connections(10)
         .connect(&database_url)
         .await
         .expect("Failed to create pool");
@@ -23,6 +23,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(actix_web::web::Data::new(pool.clone()))
+            .wrap(actix_web::middleware::Logger::default())
             .configure(api::config)
             .service(fs::Files::new("/", "./statics").index_file("index.html"))
     })
